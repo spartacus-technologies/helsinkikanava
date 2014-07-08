9cb687ae4b48d10d6dfd3988765ed079aad3ee00
Eetu <eetu.prehti@gmail..com>
2014-07-08 19:19:30 +0300

    Toka!

--- src/com/example/helsinkikanava/MainActivity.java
+++ src/com/example/helsinkikanava/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
+    @Override 
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
