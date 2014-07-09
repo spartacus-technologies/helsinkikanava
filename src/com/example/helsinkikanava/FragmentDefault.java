773dd373d2f32903d4cc88daf9a47759c70d2867
Eetu <eetu.prehti@gmail..com>
2014-07-09 23:30:56 +0300

    Created 1st version of year navigation in meetings view.

--- src/com/example/helsinkikanava/FragmentDefault.java
+++ src/com/example/helsinkikanava/FragmentDefault.java
@@ -8,24 +8,31 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 //Example class for demostrating Fragment. -Eetu
-public class FragmentDefault extends Fragment implements OnClickListener {
+public class FragmentDefault extends Fragment implements OnClickListener, OnTouchListener {
 
 	Context parent_ = null;
-	View rootView = null;
+	View rootView = null;					//Owns all fragment Views
+	Scroller scroller = new Scroller();  	//For scrolling year view
+	int scroll_speed = 7;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -33,8 +40,13 @@ public class FragmentDefault extends Fragment implements OnClickListener {
     	
         rootView = inflater.inflate(R.layout.fragment_meetings, container, false);
         //((Button)rootView.findViewById(R.id.button1)).setOnClickListener(this);
-        //((Button)getView().findViewById(R.id.button1)).setOnClickListener(this);
+        ((Button)rootView.findViewById(R.id.fragment_meetings_button_left)).setOnClickListener(this);
+        ((Button)rootView.findViewById(R.id.fragment_meetings_button_right)).setOnClickListener(this);
         
+        ((Button)rootView.findViewById(R.id.fragment_meetings_button_left)).setOnTouchListener(this);
+        ((Button)rootView.findViewById(R.id.fragment_meetings_button_right)).setOnTouchListener(this);
+        //rootView.findViewById(R.id.fragment_meetings_button_left)
+   
         generateYearNavigation();
         return rootView;
     }
@@ -48,35 +60,39 @@ public class FragmentDefault extends Fragment implements OnClickListener {
     	
     	Log.i("asdas", "adfsd");
     	
-    	//TODO
-    	int min_year = 2011;
-    	int max_year = 2014;
-
-    	Button test = new Button(getActivity());
-    	test.setBackgroundColor(Color.BLACK);
-    	test.setText("Uusimmat");
-    	test.setTextColor(Color.WHITE);
     	
-    	ImageButton imgbutton = new ImageButton (getActivity()); 
-        
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
-        		((int) LayoutParams.WRAP_CONTENT, (int) LayoutParams.WRAP_CONTENT);
-        		
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);
-        
-        imgbutton.setLayoutParams(params);
-        imgbutton.setImageResource(R.drawable.btn_uusimmat);
-        imgbutton.setPadding(0, 0, 0, 0);
-        LinearLayout my_root = (LinearLayout) rootView.findViewById(R.id.fragment_meetings_years);
-    	my_root.addView(test, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-    	//my_root.addView(test, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+    	int year_index = 2014;		//TODO
+    	int min_year = 2008;		//TODO
+
+    	while(year_index >= min_year){
+    	
+	    	Button test = new Button(getActivity());
+	    	test.setBackgroundColor(Color.BLACK);
+	    	test.setText(Integer.toString(year_index));
+	    	test.setTextColor(Color.WHITE);
+	    	
+	    	ImageButton imgbutton = new ImageButton (getActivity()); 
+	        
+	        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+	        		
+	        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+	        
+	        imgbutton.setLayoutParams(params);
+	        imgbutton.setImageResource(R.drawable.btn_uusimmat);
+	        imgbutton.setPadding(0, 0, 0, 0);
+	        
+	        LinearLayout my_root = (LinearLayout) rootView.findViewById(R.id.fragment_meetings_years);
+	    	my_root.addView(test, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+	    	
+	    	--year_index;
+    	}
     }
     
 	@Override
 	public void onClick(View v) {
 
 		switch (v.getId()) {
-		 
+		
 		case R.id.button1: 
 
 			((TextView)getView().findViewById(R.id.textView_current_version_value)).setText(AutoUpdater.getCurrentVersion(parent_));
@@ -84,9 +100,112 @@ public class FragmentDefault extends Fragment implements OnClickListener {
 
 			break;
 
+		//Scroll left:
+		case R.id.fragment_meetings_button_left: 
+			
+			break;
+			
+		//Scroll right:	
+		case R.id.fragment_meetings_button_right: 
+			
+			break;
 		default:
 			break;
 		}
 		
 	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+				
+		switch (v.getId()) {
+		
+		case R.id.fragment_meetings_button_left:
+			
+	        scroller.setDirection(-scroll_speed);
+			if(event.getAction() == android.view.MotionEvent.ACTION_DOWN){
+				
+
+				scroller.start();
+			}
+			else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+				
+				scroller.stop();
+			}
+			
+			break;
+			
+		case R.id.fragment_meetings_button_right:
+			
+	        scroller.setDirection(scroll_speed);
+			if(event.getAction() == android.view.MotionEvent.ACTION_DOWN){
+				
+
+				scroller.start();
+			}
+			else if(event.getAction() == android.view.MotionEvent.ACTION_UP){
+				
+				scroller.stop();
+			}
+			
+			break;
+			
+			
+		default:
+			break;
+		}
+		
+		Log.i("FragmentDefault", event.toString());
+		//Log.i("FragmentDefault", "touch");
+		
+		v.performClick();
+		return false;
+	}
+	
+	class Scroller{		//For scrolling year list
+		
+		final Handler mHandler = new Handler();
+		private int mInterval = 10;
+		
+		boolean run = false;
+		int direction_ = 0;
+		
+		public void start(){
+			
+			run  = true;
+			runner.run();
+		}
+		public void stop(){
+			
+			run = false;
+		}
+		
+		public void setDirection(int direction){
+			
+			direction_ = direction;
+		}
+		
+		public 
+		
+		Runnable runner = new Runnable() {
+			
+		    @Override 
+		    public void run() {
+		    	
+		    	Log.i("Scroller", "running...");
+		    	
+		    	mHandler.postDelayed(this, mInterval);
+		    	((HorizontalScrollView)rootView.findViewById(R.id.horizontalScrollView_years)).scrollBy(direction_, 0);
+		    	
+				if(!run){
+					
+					Log.w("Scroller", "quitting...");
+					mHandler.removeCallbacks(this);
+					//finish();
+				}
+		    }
+		};
+		
+	}
+	
 }
